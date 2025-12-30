3e1b4f78b91266efa726892dcae25d341d33e82e
chandana <chanduvadla499@gmail.com>
2025-12-30 06:58:59 +0000

    updated not found

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,8 +1,16 @@
 const NotFound = () => (
-  <img
-    src="https://assets.ccbp.in/frontend/react-js/page-not-found.png"
-    alt="not found"
-  />
+  <>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/page-not-found.png"
+      alt="PAGE NOT FOUND"
+    />
+    <h1>PAGE NOT FOUND</h1>
+    <p>
+      we're sorry, the page you requested could not be foundPlease go back to
+      the homepage
+    </p>
+    <button type="button">Go to Home</button>
+  </>
 )
 
 export default NotFound
