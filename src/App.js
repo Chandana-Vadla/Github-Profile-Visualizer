014f26faf57db9e7d16e2c3e5f6c1ead23c000b9
chandana <chanduvadla499@gmail.com>
2025-12-31 14:16:21 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, useLocation} from 'react-router-dom'
 
 import Header from './components/Header'
 import Home from './components/Home'
@@ -6,23 +6,36 @@ import Repositories from './components/Repositories'
 import RepositoryItemDetails from './components/RepositoryItemDetails'
 import Analysis from './components/Analysis'
 import NotFound from './components/NotFound'
+
 import './App.css'
 
-const App = () => (
-  <>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/repositories" component={Repositories} />
-      <Route
-        exact
-        path="/repositories/:repoName"
-        component={RepositoryItemDetails}
-      />
-      <Route exact path="/analysis" component={Analysis} />
-      <Route component={NotFound} />
-    </Switch>
-  </>
-)
+const App = () => {
+  const location = useLocation()
+
+  // Hide header ONLY on Home route (small devices via CSS)
+  const hideHeaderOnMobileHome = location.pathname === '/'
+
+  return (
+    <div
+      className={`app-container ${
+        hideHeaderOnMobileHome ? 'hide-header-mobile' : ''
+      }`}
+    >
+      <Header />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/repositories" component={Repositories} />
+        <Route
+          exact
+          path="/repositories/:repoName"
+          component={RepositoryItemDetails}
+        />
+        <Route exact path="/analysis" component={Analysis} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  )
+}
 
 export default App
